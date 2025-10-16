4706e0a6bfef04a8a7438d466cec302c89452541
Syed <xayanmay@gmail.com>
2025-10-16 10:31:35 -0400

    tip 07

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //write your code here
+//"hello world hello" -> {"hello":2, "world": 1}
+const countWordFrequency = (sentence) => {
+  // Split the sentence into an array of words
+  // "hello world" -> ["hello", "world", "hello"]
+  const words = sentence.split(" ");
+
+  // Create frequency object to store { word: frequency } pairs
+  const frequency = {};
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (frequency[word] === undefined) {
+      frequency[word] = 1;
+    } else {
+      frequency[word]++;
+    }
+  }
+  return frequency;
+};
+
+// Test the function
+console.log(countWordFrequency("hello world hello"));
+console.log(countWordFrequency("the cat and the hat"));
+// { the: 2, cat: 1, and: 1, hat: 1 }
